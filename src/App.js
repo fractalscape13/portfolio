768075bd838592b4a2fb431850b8307f024a84ce
Joseph Wangemann <fractalscape13@gmail.com>
2021-06-02 12:12:53 -0600

    create files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Projects from './Projects';
+import Contact from './Contact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+export default () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <div className={'header'}>
+          <Header />
+        </div>
+        <div className='mainStyle'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/projects' component={Projects} />
+            <Route path='/contact' component={Contact} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
-
-export default App;
